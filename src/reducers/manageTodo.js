caffcabc790810b48ad445e8604282d81a61a12c
Teodoro Garcia <tedtke514@gmail.com>
2021-04-05 13:45:40 -0500

    commit tree 4be282f5d28414ac62609a9440b6126f86a8f4b9

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,12 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      console.log({ todos: state.todos.concat(action.payload.text)});
 
-  console.log("reducer received this action:", action);
-
-  return state;
+      return{ todos: state.todos.concat(action.payload.text) }
+  default:
+    return state;
+  }
 }
